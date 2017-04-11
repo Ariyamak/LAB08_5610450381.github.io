bb10e08eaa45c7c560dc76122fc41134649520f0
Meng <Ariyamak.p@ku.th>
2017-04-11 14:55:04 +0700

    lab08 detail

---
+++ index.php
@@ -0,0 +1,46 @@
+<html>
+<head>
+<title>NisitKU</title>
+
+<link rel="stylesheet" type="text/css" href="CSS.css">
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.0/jquery.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+
+</head>
+<body class="pageForm">
+<h1 class="header">Mr.Ariyamak<sub><sub> 5610450381  </sub></sub></h1>
+
+
+<div id="LoginMain">
+
+<h1 id="loginTitle">RESUME</h1>
+<nav class="navbar navbar-inverse">
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <a class="navbar-brand" href="#">MY HOMEWORK</a>
+    </div>
+    <ul class="nav navbar-nav">
+      <li class="active"><a href="index.php">Home</a></li>
+      <li><a href="/5610450381_LAB07/lab01/index.html">LAB_01</a></li>
+      <li><a href="/5610450381_LAB07/lab02/index.html">LAB_02</a></li>
+      <li><a href="/5610450381_LAB07/lab03/index.html">LAB_03</a></li>
+      <li><a href="/5610450381_LAB07/lab04/index.html">LAB_04</a></li>
+      <li><a href="/5610450381_LAB07/lab05/index.html">LAB_05</a></li>
+      <li><a href="/5610450381_LAB07/lab06/index1.php">LAB_06.1</a></li>
+      <li><a href="/5610450381_LAB07/lab06/index2.php">LAB_06.2</a></li>
+    </ul>
+  </div>
+</nav>
+<form action = "#" name = "loginForm" method = "post">
+<img src="resume1.gif">
+</form>
+</div>
+
+
+
+
+</body>
+</html>
